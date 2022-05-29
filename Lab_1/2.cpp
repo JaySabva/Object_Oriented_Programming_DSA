b994fcafb3ec9f4287264960eb2d68ac1c10c2ed
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-05-29 18:43:23 +0530

    Create 2.cpp

---
+++ Lab_1/2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+void fun(int &x,int &y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+int main()
+{
+   int x,y;
+   cout<<"Enter value of x\n";
+   cin>>x;//input x
+   cout<<"Enter value of y\n";
+   cin>>y;//input y
+   fun(x,y);//function calling
+   cout<<"Values after swaping\n";
+   cout<<"Value Of X="<<x<<"\n"<<"Value Of Y="<<y;//value after swaping
+   
+    return 0;
+}
