1b79b6b21ccecf80e1d2405ba67aa863aa79db3d
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-05-30 09:13:53 +0530

    Update 2.cpp

--- Lab_1/2.cpp
+++ Lab_1/2.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 
 using namespace std;
-void fun(int &x,int &y)
-{
-    int temp;
-    temp=x;
-    x=y;
-    y=temp;
+void fun(int & x, int & y) {
+   int temp;
+   temp = x;
+   x = y;
+   y = temp;
 }
-int main()
-{
-   int x,y;
-   cout<<"Enter value of x\n";
-   cin>>x;//input x
-   cout<<"Enter value of y\n";
-   cin>>y;//input y
-   fun(x,y);//function calling
-   cout<<"Values after swaping\n";
-   cout<<"Value Of X="<<x<<"\n"<<"Value Of Y="<<y;//value after swaping
-   
-    return 0;
+int main() {
+   int x, y;
+   cout << "Enter value of x\n";
+   cin >> x; //input x
+   cout << "Enter value of y\n";
+   cin >> y; //input y
+   fun(x, y); //function calling
+   cout << "Values after swaping\n";
+   cout << "Value Of X=" << x << "\n" << "Value Of Y=" << y; //value after swaping
+
+   return 0;
 }
