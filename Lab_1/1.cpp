b9daab66476b2fb9f66efc50a118e2b9a7336949
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-05-30 09:13:28 +0530

    Update 1.cpp

--- Lab_1/1.cpp
+++ Lab_1/1.cpp
@@ -2,16 +2,14 @@
 
 using namespace std;
 
-int main()
-{
-    int arr[5],sum=0;
-    float avg;
-    for(int i=0;i<5;i++)
-    {
-        cin>>arr[i];//scaning input
-        sum+=arr[i];//sum
-    }
-    avg=sum/5.0;
-    cout<<"sum is "<<sum<<"\n"<<"avg is "<<avg;//print
-    return 0;
+int main() {
+   int arr[5], sum = 0;
+   float avg;
+   for (int i = 0; i < 5; i++) {
+      cin >> arr[i]; //scaning input
+      sum += arr[i]; //sum
+   }
+   avg = sum / 5.0;
+   cout << "sum is " << sum << "\n" << "avg is " << avg; //print
+   return 0;
 }
