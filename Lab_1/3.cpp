632377065f9054bf198dc61de0da1e936cee9024
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-05-30 09:14:22 +0530

    Update 3.cpp

--- Lab_1/3.cpp
+++ Lab_1/3.cpp
@@ -1,41 +1,38 @@
 #include<iostream>
+
 using namespace std;
 //this can be done by operator overloading
-class complex
-{
-    public:
-    int real,imaginary;
+class complex {
+   public:
+      int real, imaginary;
 
-    complex add(complex c1,complex c2)
-    {
-        complex x1;
-        x1.real=c1.real+c2.real;
-        x1.imaginary=c1.imaginary+c2.imaginary;
-        return x1;
-    }
+   complex add(complex c1, complex c2) {
+      complex x1;
+      x1.real = c1.real + c2.real;
+      x1.imaginary = c1.imaginary + c2.imaginary;
+      return x1;
+   }
 
-    complex multiply(complex c1,complex c2)
-    {
-        complex x1;
-        x1.real=c1.real*c2.real-c1.imaginary*c2.imaginary;
-        x1.imaginary=c1.imaginary*c2.real+c2.imaginary*c1.real;
-        return x1;
-    }
+   complex multiply(complex c1, complex c2) {
+      complex x1;
+      x1.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
+      x1.imaginary = c1.imaginary * c2.real + c2.imaginary * c1.real;
+      return x1;
+   }
 };
 
-int main()
-{
-    complex c1,c2,c3,c4;
-    cout<<"Enter 1st Complex Number:\n";
-    cin >> c1.real >> c1.imaginary;
-    cout<<"Enter 2nd Complex Number:\n";
-    cin>>c2.real >> c2.imaginary;
-    
-    c3=c3.add(c1,c2);
-    c4=c4.multiply(c1,c2);
-    
-    cout << "Addition Of Two Complex Number = " << c3.real << "+(" << c3.imaginary << ")i "<< endl;
-    cout << "Multiplication Of Two Complex Number = " << c4.real << "+(" << c4.imaginary << ")i " ;
-    
-    return 0; 
+int main() {
+   complex c1, c2, c3, c4;
+   cout << "Enter 1st Complex Number:\n";
+   cin >> c1.real >> c1.imaginary;
+   cout << "Enter 2nd Complex Number:\n";
+   cin >> c2.real >> c2.imaginary;
+
+   c3 = c3.add(c1, c2);
+   c4 = c4.multiply(c1, c2);
+
+   cout << "Addition Of Two Complex Number = " << c3.real << "+(" << c3.imaginary << ")i " << endl;
+   cout << "Multiplication Of Two Complex Number = " << c4.real << "+(" << c4.imaginary << ")i ";
+
+   return 0;
 }
