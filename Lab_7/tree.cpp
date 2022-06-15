eef20b28b3df330b9470e5be04453ef7c7a156c0
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-06-15 22:02:46 +0530

    Update tree.cpp

--- Lab_7/tree.cpp
+++ Lab_7/tree.cpp
@@ -1,5 +1,10 @@
-#include<iostream>
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                         Author- Sabva Jay Dilipbhai                                                //
+//                                         ID-202101224                                                               //
+//                                         Implementation of binary tree                                              //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include<iostream>
 using namespace std;
 class binarytree;
 class node;
@@ -13,10 +18,6 @@ class queue {
       rear = -1;
    }
    void push(node * temp) {
-      if (rear == size - 1) {
-         cout << "Queue is full" << endl;
-         return;
-      }
       if (front == -1) {
          front = 0;
       }
@@ -24,10 +25,6 @@ class queue {
       arr[rear] = temp;
    }
    void pop() {
-      if (front == -1) {
-         cout << "Queue is empty" << endl;
-         return;
-      }
       if (front == rear) {
          front = rear = -1;
          return;
@@ -156,15 +153,19 @@ class binarytree {
       }
    }
    void deletions(int data) {
+    if (root == NULL) {
+        cout << "Tree is empty" << endl;
+        return;
+    }
       queue q;
       q.push(root);
       node * temp;
-      node * key_node = NULL;
+      node * data_node = NULL;
       while (!q.empty()) {
          temp = q.front1();
          q.pop();
          if (temp -> data == data) {
-            key_node = temp;
+            data_node = temp;
          }
          if (temp -> left != NULL) {
             q.push(temp -> left);
@@ -173,13 +174,19 @@ class binarytree {
             q.push(temp -> right);
          }
       }
-      if (key_node == NULL) {
-         cout << "Key not found" << endl;
+      if (data_node == NULL) {
+         cout << "Data not found" << endl;
          return;
+      }
+      if(data_node==root&&data_node->left==NULL&&data_node->right==NULL) 
+      {
+            root=NULL;
+            delete data_node;
+            return;
       }
       int x = temp -> data;
       deletDeepest(temp);
-      key_node -> data = x;
+      data_node -> data = x;
 
    }
 };
@@ -207,30 +214,62 @@ int main() {
          break;
       case 2:
         cout << "Inorder traversal" << endl;
+        if(bt.root==NULL)
+        {
+            cout<<"Tree is empty"<<endl;
+        }
+        else{
          bt.inorder(bt.root);
          cout << endl;
+        }
          break;
       case 3:
          cout << "Preorder traversal" << endl;
+         if(bt.root==NULL)
+        {
+            cout<<"Tree is empty"<<endl;
+        }
+        else{
          bt.preorder(bt.root);
          cout << endl;
+        }
          break;
       case 4:
+      if(bt.root==NULL)
+        {
+            cout<<"Tree is empty"<<endl;
+        }
+        else{
         cout << "Postorder traversal" << endl;
          bt.postorder(bt.root);
          cout << endl;
+        }
          break;
       case 5:
         cout << "Levelorder traversal" << endl;
+        if(bt.root==NULL)
+        {
+            cout<<"Tree is empty"<<endl;
+        }
+        else{
          bt.levelordertraversal(bt.root);
          cout << endl;
+        }
          break;
       case 6:
+      if(bt.root==NULL)
+      {
+         cout<<"Tree is empty"<<endl;
+         break;
+      }
+      else{
          int data;
          cout << "Enter the data to be deleted" << endl;
          cin >> data;
-         bt.deletions(data);
+         bt.deletions(data);}
          break;
+        case 7:
+            break;
       default:
          cout << "Invalid choice" << endl;
       }
